bd53da87fc5a90825fb392d3a9d370757d6d126d
SachinPerera20 <sachinnipun20@gmail.com>
2026-01-06 22:39:26 +0530

    first commit with creating project using react vite typescript and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Starting Freelance platform
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
