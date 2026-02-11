9d0f37faff13218eb7d44e226e8097846d3d2b74
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-12 00:45:02 +0530

    login page2

---
+++ src/pages/Login.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-2xl font-semibold">Login</h1>
+      </div>
+    );
+  }
+  
