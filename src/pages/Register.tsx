9d4f0770dc4bb2ab41d1f9636be87ea1ee60c692
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-13 16:55:33 +0530

    register 9

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -30,6 +30,26 @@ export default function Register() {
             <div className="max-w-md mx-auto mt-20 bg-white p-8 rounded shadow">
             <h2 className="text-2xl font-semibold mb-6 text-center">Register</h2>
 
+            <form onSubmit={handleSubmit} className="space-y-4">
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="w-full border rounded px-3 py-2"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required />
+
+         <input
+          type="email"
+          placeholder="Email"
+          className="w-full border rounded px-3 py-2"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        /> 
+
+        </form>
+
 
                 </div>
           );
