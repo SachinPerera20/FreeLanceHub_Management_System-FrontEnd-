c97ecc9a82c6cd6dc39f1b6e4a192f5f9e46c6e4
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 01:14:00 +0530

    createjob page1

--- src/pages/jobs/CreateJobPage.tsx
+++ src/pages/jobs/CreateJobPage.tsx
@@ -1,12 +1,1 @@
-export default function CreateJobPage() {
-    return (
-      <div className="rounded-xl border bg-white p-6 shadow-sm">
-        <h1 className="text-xl font-semibold text-gray-900 mb-4">
-          Create New Job
-        </h1>
-        <p className="text-gray-600">
-          Job creation form will be implemented here.
-        </p>
-      </div>
-    );
-  }
+import { useState } from 'react';
