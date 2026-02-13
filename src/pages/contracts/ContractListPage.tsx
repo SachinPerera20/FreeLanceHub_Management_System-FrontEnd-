9f77d85bc2570f66a3b6a4d3cca7b181fde2c53b
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-13 21:08:20 +0530

    contractlist

---
+++ src/pages/contracts/ContractListPage.tsx
@@ -0,0 +1,12 @@
+export default function ContractListPage() {
+    return (
+      <div className="rounded-xl border bg-white p-6 shadow-sm">
+        <h1 className="text-xl font-semibold text-gray-900 mb-4">
+          Contracts
+        </h1>
+        <p className="text-gray-600">
+          User contracts will be listed here.
+        </p>
+      </div>
+    );
+  }
