47bcccd541f92c2b4bb5d1d50a003fa3bdb444d3
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 01:16:40 +0530

    createjob page5

--- src/pages/jobs/CreateJobPage.tsx
+++ src/pages/jobs/CreateJobPage.tsx
@@ -2,3 +2,15 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useJobs } from '../../hooks/useJobs';
 import { useAuth } from '../../hooks/useAuth';
+
+export default function CreateJobPage() {
+  const navigate = useNavigate();
+  const { addJob } = useJobs();
+  const { user } = useAuth();
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [budget, setBudget] = useState('');
+
+
+}
