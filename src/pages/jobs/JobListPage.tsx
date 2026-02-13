75f644c62e4361e98850bed66115dc076499af0e
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-13 21:05:01 +0530

    joblist page

---
+++ src/pages/jobs/JobListPage.tsx
@@ -0,0 +1,10 @@
+export default function JobListPage() {
+    return (
+      <div className="rounded-xl border bg-white p-6 shadow-sm">
+        <h1 className="text-xl font-semibold text-gray-900 mb-4">Job Listings</h1>
+        <p className="text-gray-600">
+          This is where available jobs will be displayed.
+        </p>
+      </div>
+    );
+  }
