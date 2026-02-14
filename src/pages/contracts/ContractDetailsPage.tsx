11f9ec5da91a9d99941145da561665e970a40f8a
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-15 00:42:48 +0530

    contract detailspage4

--- src/pages/contracts/ContractDetailsPage.tsx
+++ src/pages/contracts/ContractDetailsPage.tsx
@@ -1,3 +1,12 @@
 import { Link, useParams } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import { useContracts } from "../../context/ContractsContext";
+
+export default function ContractDetailsPage() {
+    const { contractId } = useParams();
+    const { user } = useAuth();
+    const { getContractById } = useContracts();
+
+
+
+}  
