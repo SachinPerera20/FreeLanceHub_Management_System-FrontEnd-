5f6fa0c7cbf98a588edc2da139532ab6435f0db2
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 11:31:40 +0530

    jobdetails page 3

--- src/pages/jobs/JobDetailsPage.tsx
+++ src/pages/jobs/JobDetailsPage.tsx
@@ -1,2 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useJobs } from "../../hooks/useJobs";
+
+export default function JobDetailsPage() {
+
+    const { jobId } = useParams();
+    const { jobs } = useJobs();
+
+
+}
