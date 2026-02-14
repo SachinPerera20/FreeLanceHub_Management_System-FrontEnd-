ff1a11c94a6598e2a3d8bd05f2657c4b014eab07
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 16:05:26 +0530

    proposal submit page6

--- src/pages/jobs/SubmitProposalPage.tsx
+++ src/pages/jobs/SubmitProposalPage.tsx
@@ -3,3 +3,21 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useJobs } from "../../hooks/useJobs";
 import { useAuth } from "../../hooks/useAuth";
 import { useProposals } from "../../context/ProposalsContext";
+
+export default function SubmitProposalPage() {
+
+    const { id } = useParams(); 
+    const navigate = useNavigate();
+  
+    const { jobs } = useJobs();
+    const { user } = useAuth();
+    const { addProposal } = useProposals();
+  
+    const job = useMemo(() => jobs.find((j) => j.id === id), [jobs, id]);
+  
+    const [coverLetter, setCoverLetter] = useState("");
+    const [proposedBudget, setProposedBudget] = useState<number>(job?.budget ?? 0);
+    const [estimatedDays, setEstimatedDays] = useState<number>(7);
+    const [error, setError] = useState<string>("");
+
+}
