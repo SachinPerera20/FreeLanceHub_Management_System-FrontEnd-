d70469c58f66d568110469c243cebd019354420a
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 16:03:37 +0530

    proposal submit page4

--- src/pages/jobs/SubmitProposalPage.tsx
+++ src/pages/jobs/SubmitProposalPage.tsx
@@ -1,3 +1,4 @@
 import { useMemo, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useJobs } from "../../hooks/useJobs";
+import { useAuth } from "../../hooks/useAuth";
