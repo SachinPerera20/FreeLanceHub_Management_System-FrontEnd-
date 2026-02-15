9c1ec81b3fda9ec8ee4c7b460ba3bdfb0f2e391f
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-15 22:55:02 +0530

    client access5

--- src/context/proposals.context.ts
+++ src/context/proposals.context.ts
@@ -1,12 +1,10 @@
-// src/context/proposals.context.ts
-
 import type { Proposal } from "../types/proposal.types";
+
+export type NewProposalInput = Omit<Proposal, "id" | "status" | "createdAt">;
 
 export type ProposalsContextValue = {
   proposals: Proposal[];
-  addProposal: (data: Omit<Proposal, "id" | "createdAt" | "status">) => void;
+  addProposal: (data: NewProposalInput) => void;
   getProposalsByJobId: (jobId: string) => Proposal[];
-
   updateProposalStatus: (proposalId: string, status: Proposal["status"]) => void;
-
 };
